44fb9795f49d55f2859950490bfd94f1bdb91645
Colin Wood <woodcolin38@gmail.com>
2024-12-10 15:40:23 -0700

    other people's lint

--- q2_cutadapt/tests/test_demux.py
+++ q2_cutadapt/tests/test_demux.py
@@ -768,16 +768,16 @@ class TestDemuxPaired(TestPluginBase):
                                      mixed_orientation=True)
         exp = [
             # sample_a fwd
-            '@id1\nACGTACGT\n+\nyyyyyyyy\n' \
+            '@id1\nACGTACGT\n+\nyyyyyyyy\n'
             '@id3\nACGTACGT\n+\nyyyyyyyy\n',
             # sample_a rev
-            '@id1\nTGCATGCATGCA\n+\nzzzzzzzzzzzz\n' \
+            '@id1\nTGCATGCATGCA\n+\nzzzzzzzzzzzz\n'
             '@id3\nTGCATGCATGCA\n+\nzzzzzzzzzzzz\n',
             # sample_b fwd
-            '@id4\nACGTACGT\n+\nyyyyyyyy\n' \
+            '@id4\nACGTACGT\n+\nyyyyyyyy\n'
             '@id2\nACGTACGT\n+\nyyyyyyyy\n',
             # sample_b rev
-            '@id4\nTGCATGCATGCA\n+\nzzzzzzzzzzzz\n' \
+            '@id4\nTGCATGCATGCA\n+\nzzzzzzzzzzzz\n'
             '@id2\nTGCATGCATGCA\n+\nzzzzzzzzzzzz\n',
             # sample_c fwd
             '@id5\nACGTACGT\n+\nyyyyyyyy\n',
@@ -786,7 +786,8 @@ class TestDemuxPaired(TestPluginBase):
             # sample_d fwd
             '@id6\nACGTACGT\n+\nyyyyyyyy\n',
             # sample_d rev
-            '@id6\nTGCATGCATGCA\n+\nzzzzzzzzzzzz\n', ]
+            '@id6\nTGCATGCATGCA\n+\nzzzzzzzzzzzz\n',
+        ]
 
         # We want to be sure that the validation is 100%, not just `min`,
         obs_demuxed_art.validate(level='max')
